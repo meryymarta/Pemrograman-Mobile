e70612b409448ef69ce3f5a261ac49d0d526515f
meryymarta <meriammartadinata@gmail.com>
2026-04-15 13:49:37 +0700

    praktikum 2: implementasi button row

--- WEEK 6-FLUTTER 2/src/layout_flutter/lib/main.dart
+++ WEEK 6-FLUTTER 2/src/layout_flutter/lib/main.dart
@@ -7,18 +7,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
     Widget titleSection = Container(
-      padding: const EdgeInsets.all(32), // Soal 3: Padding 32 piksel di semua sisi
+      padding: const EdgeInsets.all(32),
       child: Row(
         children: [
           Expanded(
-            /* soal 1 */
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start, 
               children: [
-                /* soal 2 */
                 Container(
-                  padding: const EdgeInsets.only(bottom: 8), // Padding bawah 8
+                  padding: const EdgeInsets.only(bottom: 8),
                   child: const Text(
                     'Wisata Gunung di Batu',
                     style: TextStyle(
@@ -29,20 +29,28 @@ class MyApp extends StatelessWidget {
                 Text(
                   'Batu, Malang, Indonesia',
                   style: TextStyle(
-                    color: Colors.grey[500], // Warna teks abu-abu
+                    color: Colors.grey[500],
                   ),
                 ),
               ],
             ),
           ),
-          /* soal 3 */
           Icon(
             Icons.star,
-            color: Colors.red[500],
+            color: Colors.red[500], 
           ),
-          const Text('41'),
+          const Text('41'), 
         ],
       ),
+    );
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
     );
 
     return MaterialApp(
@@ -51,13 +59,34 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        // Mengganti 'Hello World' dengan titleSection
         body: Column(
           children: [
             titleSection,
+            buttonSection,
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
